9570ec870ce9ca87d9f0bac5ad60003994daa0f8
edledsonganso <edledsonganso@gmail.com>
2026-07-30 11:42:39 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+const express = require("express");
 
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("🦢 Goose Bot AI Online!");
+});
+
+app.post("/interactions", (req, res) => {
+    console.log(req.body);
+
+    return res.json({
+        type: 1
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
